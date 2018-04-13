ed45d1f2bc4b8a991d22e47c51c709cf70ad46d5
Anton Medvedev <anton@medv.io>
2018-04-13 20:00:35 +0700

    port env

--- index.js
+++ index.js
@@ -45,5 +45,6 @@ app.use(route.get('/status', async (ctx) => {
   }
 }))
 
-app.listen(3000)
-console.log('App started on port 3000')
+const port = process.env.PORT || 3000
+app.listen(port)
+console.log('App started on port ' + port)
